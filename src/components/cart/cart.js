e6462dc179e4aa75e5f3d0bf4dad63cf8f992ddf
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-21 20:00:47 +0100

    restClient abstraction & Cart item quantity

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -7,7 +7,10 @@ class Cart extends React.Component {
         <div className="innerBox">
           <center><h4>Cart</h4></center>
           {this.props.cart.map((item) => (
-            <div className="sep" key={item.id}>{item.name}</div>
+            <div className="sep" key={item.id}>
+              <div className="floatLeft cartQuantity">{item.quantity}</div>
+              {item.name}
+            </div>
           ))}
         </div>
     )
