47164d977abfc8e5b0a310c838336826d3baee6e
davidgfolch@gmail.com <davidgfolch@gmail.com>
2020-03-30 09:42:42 +0200

    travis sonarcloud with jest-sonar-reporter

--- sonar-project.js
+++ sonar-project.js
@@ -14,7 +14,7 @@ sonarqubeScanner(
         "sonar.organization": "davidgfolch-github",
         "sonar.sources": "./src",
         // "sonar.exclusions": "**/__tests__/**",
-        "sonar.tests": "./src/**/*.test.js",
+        "sonar.tests": "./src/*.test.js,./src/**/*.test.js",
         // "sonar.test.inclusions": "./src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts",
         "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
         "sonar.testExecutionReportPaths": "test-report.xml",
